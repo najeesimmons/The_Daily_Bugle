ee6f4c928a83800b9c928b9e852af4be84c32337
najeesimmons <najee.bryant.simmons@gmail.com>
2022-02-08 11:18:37 -0500

    added main_app

---
+++ The_Daily_Bugle/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for The_Daily_Bugle project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'The_Daily_Bugle.settings')
+
+application = get_asgi_application()
