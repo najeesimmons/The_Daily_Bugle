694009d22d4982298fff0ac3f8ce924ba5980ea8
najeesimmons <najee.bryant.simmons@gmail.com>
2022-02-08 12:34:51 -0500

    added home and about url routes, view classes and templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 # this like app.use() in express
-urlpatterns = urlpatterns = [
-    path('', views.Home.as_view(), name="home")
+urlpatterns = [
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
 ]
