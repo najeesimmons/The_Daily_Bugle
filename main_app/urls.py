b24cd335c64f880dd841a8b6fa1d7b43ebcaff46
najeesimmons <najee.bryant.simmons@gmail.com>
2022-02-08 12:09:07 -0500

    imported Views class/ extended with Home

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 # this like app.use() in express
-urlpatterns = [
-
+urlpatterns = urlpatterns = [
+    path('', views.Home.as_view(), name="home")
 ]
